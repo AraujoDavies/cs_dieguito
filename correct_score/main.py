ddf4c88bf5ae02d1494666528ee6ce70ed5beb12
Davies <daviesaraujo@hotmail.com>
2023-03-26 23:14:05 -0300

    implementando resultado do dia e melhorando looping de execução para não travar quando acabar jogos do dia

--- correct_score/main.py
+++ correct_score/main.py
@@ -5,19 +5,42 @@ from defs_analise_jogos_dia import analise_jogos_do_dia
 from os import getenv
 from database import database
 from defs_monitoramento_entradas import monitoramento_de_entradas
-import logging
+import logging, datetime, schedule
 from betfair import Betfair
 from helpers import jogos_selecionados, data_inicio
+from resultado_do_dia import Traderbet
+from telegram import enviar_no_telegram
 
-logging.basicConfig(level=logging.WARNING, filename="cs.log", encoding='utf-8', format="%(asctime)s - %(levelname)s: %(message)s")
+logging.basicConfig(level=logging.WARNING, filename=f"logs/cs{datetime.datetime.now().strftime('%d-%m')}.log", encoding='utf-8', format="%(asctime)s - %(levelname)s: %(message)s")
 
-    
 load_dotenv()
+
+
+def atualiza_database():
+    jogos_para_analisar = betfair.lista_de_jogos()
+    jogos_selecionados(jogos_para_analisar)
+
+def resultado_do_dia(driver: webdriver):
+    a = Traderbet(driver)
+    a.login_traderbet()
+    df = a.resultado_diario(100)
+    if len(df) > 0:
+        df = df[['Jogo', 'Percentual']]
+        df.loc[len(df)] = ['Resultado do dia: ', df['Percentual'].sum()]
+        resultado_do_dia = round(df['Percentual'][len(df) - 1], 2)
+        df['Percentual'] = df['Percentual'].map(lambda x: f"{round(x,2)}%")
+        msg = df.to_string(index=False)
+        msg = " ".join(msg.split()).replace('> <','><')
+        msg = msg.replace("%", "%\n").replace("Jogo Percentual", " 🚨📚Resumo de Hoje📚🚨 \n\n").replace("Resultado do dia", "\n 📈 Resultado")
+        enviar_no_telegram(getenv('TELEGRAM_CHAT_ID'), msg)
 
 # selenium grid
 driver = webdriver.Remote(
     command_executor="http://localhost:4444",
     options=webdriver.ChromeOptions())
+
+schedule.every().day.at("22:50").do(resultado_do_dia, driver=driver)
+schedule.every().day.at("02:00").do(atualiza_database)
 
 betfair = Betfair(driver)
 
@@ -28,34 +51,26 @@ print('ajustando visualização...')
 betfair.exibindo_jogos_por_data()
 
 print('armazenando jogos do dia no database...')
-jogos_para_analisar = betfair.lista_de_jogos()
-jogos_selecionados(jogos_para_analisar)
+atualiza_database()
 
 print('Atualizando data de inicio dos jogos.')
 data_inicio(driver)
 
 # coletando dados do banco de dados para iniciar o while
-comando = f'SELECT * FROM {getenv("TABLE_JOGOS_DO_DIA")};'
-jogos_do_dia = database(comando)
 comando = f"SELECT * FROM {getenv('TABLE_ENTRADAS_EM_ANDAMENTO')}"
 entradas_em_andamento = database(comando)
 
 print('CS bot start')
 if __name__ == '__main__':
-    while bool(jogos_do_dia) or bool(entradas_em_andamento):
+    while True:
         # att variável jogos do dia
         comando = f'SELECT * FROM {getenv("TABLE_JOGOS_DO_DIA")};'
         jogos_do_dia = database(comando)
         volume_de_jogos = 0
 
         # analisando jogos
-        # try:
         volume_de_jogos = analise_jogos_do_dia(driver, jogos_do_dia) # return 0 (nenhum) 5(poucos) 10(muitos)
         driver.refresh()
-        # except:
-        #     logging.warning('falha no analise_jogos_do_dia(driver, jogos_do_dia) # return 0 (nenhum) 5(poucos) 10(muitos')
-        #     print('falha no analise_jogos_do_dia(driver, jogos_do_dia) # return 0 (nenhum) 5(poucos) 10(muitos')
-        #     sleep(30)
 
         # monitoramento das entradas feitas
         comando = f"SELECT * FROM {getenv('TABLE_ENTRADAS_EM_ANDAMENTO')}"
@@ -70,5 +85,4 @@ if __name__ == '__main__':
             logging.warning("baixo volume de jogos")
             sleep(60)
 
-    logging.warning('Sem jogos para ser analisados! Até amanhã ><')
-    driver.quit()
+        schedule.run_pending()
---
+++ correct_score/resultado_do_dia.py
@@ -0,0 +1,89 @@
+import logging
+from os import getenv
+from dotenv import load_dotenv
+import pandas as pd
+from time import sleep
+import datetime
+
+load_dotenv()
+
+class Traderbet():
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.get('https://www.tradexbr.com/customer/account/profitandloss/1')
+        self.driver.maximize_window()
+        self.user = getenv('USERNAME_TRADERBET')
+        self.password = getenv('PASSWORD_TRADERBET')
+
+
+    def login_traderbet(self):
+        """faz login na traderbet caso nao esteja logado"""
+        self.driver.implicitly_wait(10)
+        input_user = self.driver.find_elements('xpath', '//input[@placeholder="Username"]')
+        valida_url = 'tradexbr' in self.driver.current_url
+
+        if bool(input_user) and valida_url: # se não está logado e está no site da traderbet, entao logue
+            # preenchendo user
+            self.driver.find_element('xpath', '//input[@placeholder="Username"]').send_keys(self.user)
+            # preenchendo senha
+            self.driver.find_element('xpath', '//input[@placeholder="Senha"]').send_keys(self.password)
+            # submit
+            self.driver.find_element('xpath', '//span[@class="js-login biab_login-submit"]').click()
+            # aceitar termos
+            self.driver.implicitly_wait(10)
+            self.driver.find_element('xpath', '//div[@class="biab_btn-continue js-continue"]').click()
+            logging.warning('logou!')
+        else: 
+            logging.warning('já está logado!')
+
+    def resultado_diario(self, stake: int):
+        """
+            Entra na pagina de PL e retorna as entradas em um DF             
+        """
+        sleep(5)
+        # click ultimos 3 meses
+        self.driver.find_element('xpath', '//*[@id="ui-id-2-button"]').click()
+        self.driver.find_element('xpath', '//*[@id="ui-id-8"]').click()
+        sleep(5)
+        # click 100 jogos
+        self.driver.find_element('xpath', '//*[@data-value="100"]').click()
+        sleep(5)
+        
+        a = self.driver.find_element('xpath', '//div[@class="biab_table-holder"]').get_attribute('innerHTML')
+        
+        df = pd.read_html(a)
+        df = df[0]
+        df = df.rename(columns={'Mercado': 'game/market', 'Lucros e Perdas': 'PL_initial'})
+        
+        # Separando dia e hora
+        df[['Data', 'hora']] = df['Data estabelecida'].str.split(' ', expand=True)
+
+        # ajustando data
+        df[['Dia', 'prep_mes', 'ano']] = df['Data'].str.split('-', expand=True)
+        df['Mes'] = df['prep_mes']
+
+        # Calculando PL real
+        df['PL_final'] = round(df['PL_initial'].map( lambda x : x * 0.965 if x >= 0 else x), 2)
+
+        # Separando game/market
+        df[['Jogo', 'Mercado']] = df['game/market'].str.split('/ ', expand=True)
+
+        # Responsabilidade + percent
+        df['Responsabilidade'] = stake
+        df['Percentual'] = round((df['PL_final'] / df['Responsabilidade']) * 100, 2)
+
+        # Selecionando tabelas que quero
+        df['id'] = df["Jogo"] + df['Data']
+        df['id'] = df['id'].str.replace(' ', '')
+        df = df[['id', 'Data', 'Data estabelecida', 'Mes', 'Jogo', 'Mercado', 'PL_initial', 'Responsabilidade', 'PL_final', 'Percentual']]
+
+        # if mes != None:        
+        #     seletor = (df['Mes'] == str(mes)) & (df['Mercado'] == 'Placar Exato')
+        #     df = df[seletor]
+
+        hoje = datetime.datetime.now().strftime("%d-%m-%Y")
+        seletor =  df['Data'] == hoje
+        # retorna dia atual
+        df = df[seletor]
+
+        return df
